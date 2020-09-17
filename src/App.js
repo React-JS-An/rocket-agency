2c50cbd8996ae41d3d7e2c7a46e1ff895c50d965
Fernando dos Santos <nando4803@gmail.com>
2020-09-18 00:42:14 +0100

    🚀 This commit add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Navbar } from './components';
 import GlobalStyles from './globalStyles';
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyles />
       <Navbar />
-    </>
+    </Router>
   );
 }
 
