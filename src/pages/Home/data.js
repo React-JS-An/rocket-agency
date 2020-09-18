89d8b53b9f647f99c950100f57128512808f0365
Fernando dos Santos <nando4803@gmail.com>
2020-09-18 16:22:11 +0100

    🚀 This commit update data add more info

--- src/pages/Home/data.js
+++ src/pages/Home/data.js
@@ -28,6 +28,40 @@ export const homeOjectTwo = {
   description: 'Nós ajudamos empreendedores a aumentar suas receitas. Temos um Team de especialistas que podem ajudar nos objetivos da sua empresa.',
   buttonLabel: 'Registrar',
   start: '',
-  img: require('../../assets/images/profile.svg'),
+  img: require('../../assets/images/profile.jpeg'),
+  alt: 'Image'
+};
+
+export const homeOjectThree = {
+  lightBg: false,
+  primary: true,
+  lightBg: false,
+  imgStart: '',
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Agência Digital',
+  headLine: 'Líderes Especialistas em negócios Online',
+  description: 'Nós ajudamos empreendedores a aumentar suas receitas. Temos um Team de especialistas que podem ajudar nos objetivos da sua empresa.',
+  buttonLabel: 'Registrar',
+  start: '',
+  img: require('../../assets/images/svg-2.svg'),
+  alt: 'Image'
+};
+
+export const homeOjectFour = {
+  lightBg: false,
+  primary: true,
+  lightBg: false,
+  imgStart: '',
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Agência Digital',
+  headLine: 'Líderes Especialistas em negócios Online',
+  description: 'Nós ajudamos empreendedores a aumentar suas receitas. Temos um Team de especialistas que podem ajudar nos objetivos da sua empresa.',
+  buttonLabel: 'Registrar',
+  start: '',
+  img: require('../../assets/images/svg-3.svg'),
   alt: 'Image'
 };
