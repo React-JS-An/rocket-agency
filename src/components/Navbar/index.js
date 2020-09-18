7f98a77dda205bcc614c86e8ae0bbdf67d518da3
Fernando dos Santos <nando4803@gmail.com>
2020-09-18 01:32:26 +0100

    🚀 This commit update navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaTimes, FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 
 import * as S from './styles';
 
 function Navbar() {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => setClicked(!clicked);
   return (
-    <S.Nav>
-     <S.NavbarContainer>
-       <S.NavLogo>
-         <S.NavIcon />
-         ULTRA
-       </S.NavLogo>
-     </S.NavbarContainer>
-    </S.Nav>
+    <IconContext.Provider value={{color: '#FFF'}}>
+      <S.Nav>
+      <S.NavbarContainer>
+        <S.NavLogo>
+          <S.NavIcon />
+          ULTRA
+        </S.NavLogo>
+        <S.MobileIcon onClick={handleClick}>
+          {clicked ? <FaTimes /> : <FaBars />}
+        </S.MobileIcon>
+        <S.NavMenu onClick={handleClick} clicked={clicked}>
+          <S.NavItem>
+            <S.NavLinks to="/">Home</S.NavLinks>
+          </S.NavItem>
+          <S.NavItem>
+            <S.NavLinks to="/services">Services</S.NavLinks>
+          </S.NavItem>
+          <S.NavItem>
+            <S.NavLinks to="/products">Products</S.NavLinks>
+          </S.NavItem>
+        </S.NavMenu>
+      </S.NavbarContainer>
+      </S.Nav>
+    </IconContext.Provider>
   )
 }
 
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -37,3 +37,75 @@ export const NavLogo = styled(Link)`
 export const NavIcon = styled(FaMagento)`
   margin-right: 0.5rem;
 `;
+
+export const MobileIcon = styled.div`
+  display: none;
+
+  @media (max-width: 960px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+  }
+`;
+
+export const NavMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  list-style: none;
+
+  @media (max-width: 960px) {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 90vh;
+    position: absolute;
+    top: 80px;
+    left: ${({clicked}) => (clicked ? 0 : '-100%')};
+    opacity: 1;
+    transition: all 0.5s ease;
+    background: #101522;
+  }
+`;
+
+export const NavItem = styled.li`
+  height: 80px;
+  border-bottom: 2px solid transparent;
+
+  &:hover {
+    border-bottom: 2px solid #4b59f7;
+  }
+
+  @media (max-width: 960px) {
+    width: 100%;
+
+    &:hover {
+      border: none;
+    }
+  }
+`;
+
+export const NavLinks = styled(Link)`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0.5rem 1rem;
+  text-decoration: none;
+  height: 100%;
+
+  @media (max-width: 960px) {
+    text-align: center;
+    padding: 2rem;
+    height: 100%;
+    display: table;
+
+    &:hover {
+      color: #4b59f7;
+      transition: all 0.3s ease;
+    }
+  }
+`;
