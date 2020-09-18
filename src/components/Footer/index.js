71c04f2e5d41238581b265238bd6d6b32dd41751
Fernando dos Santos <nando4803@gmail.com>
2020-09-18 16:58:46 +0100

    🚀 This commit update footer add content

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,14 @@ import * as S from './styles';
 function Footer() {
   return (
     <S.Footer>
-      
+      <S.FooterSubscription>
+        <S.FooterHeading>
+          Junta-se ao nosso club de membros exclusivos para receber as últimas notícias e tendências.
+        </S.FooterHeading>
+        <S.FooterSubText>
+          Você pode subscrever-se a qualquer momento.
+        </S.FooterSubText>
+      </S.FooterSubscription>
     </S.Footer>
   )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,3 +3,9 @@ import styled from 'styled-components';
 export const Footer = styled.footer`
   
 `;
+
+export const FooterSubscription = styled.div``;
+
+export const FooterHeading = styled.h1``;
+
+export const FooterSubText = styled.p``;
