7b6ea17682ac228d82e7508c07bcbcbe3416add9
Fernando dos Santos <nando4803@gmail.com>
2020-09-18 16:18:29 +0100

    🚀 This commit update data

--- src/pages/Home/data.js
+++ src/pages/Home/data.js
@@ -11,6 +11,23 @@ export const homeOjectOne = {
   description: 'Nós ajudamos empreendedores a aumentar suas receitas. Temos um Team de especialistas que podem ajudar nos objetivos da sua empresa.',
   buttonLabel: 'Registrar',
   start: '',
-  img: '',
+  img: require('../../assets/images/svg-1.svg'),
+  alt: 'Image'
+};
+
+export const homeOjectTwo = {
+  lightBg: false,
+  primary: true,
+  lightBg: false,
+  imgStart: '',
+  lightTopLine: true,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'Agência Digital',
+  headLine: 'Líderes Especialistas em negócios Online',
+  description: 'Nós ajudamos empreendedores a aumentar suas receitas. Temos um Team de especialistas que podem ajudar nos objetivos da sua empresa.',
+  buttonLabel: 'Registrar',
+  start: '',
+  img: require('../../assets/images/profile.svg'),
   alt: 'Image'
 };
