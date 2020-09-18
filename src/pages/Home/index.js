8eab2547200b107fd758c8a3bdf3b8405dd496ab
Fernando dos Santos <nando4803@gmail.com>
2020-09-18 16:24:51 +0100

    🚀 This commit update home add datas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
-import { homeOjectOne } from './data';
+import { 
+  homeOjectOne, 
+  homeOjectTwo, 
+  homeOjectThree, 
+  homeOjectFour 
+} from './data';
 import Hero from '../../components/Hero';
 
 function Home() {
   return (
     <>
       <Hero {...homeOjectOne} />
+      <Hero {...homeOjectTwo} />
+      <Hero {...homeOjectThree} />
+      <Hero {...homeOjectFour} />
     </>
   )
 }
