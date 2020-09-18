5bc5e005765734555b8b371c4417b687efdaf9cf
Fernando dos Santos <nando4803@gmail.com>
2020-09-18 21:24:49 +0100

    🚀 This commit add scrollToTop component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { Navbar, Footer } from './components';
+import { Navbar, Footer, ScrollToTop } from './components';
 import Home from './pages/Home';
 
 import GlobalStyles from './globalStyles';
@@ -10,6 +10,7 @@ function App() {
   return (
     <Router>
       <GlobalStyles />
+      <ScrollToTop />
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
