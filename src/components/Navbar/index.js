4fdfa6b7daec8afd9f2441917cd6d6800606823a
Fernando dos Santos <nando4803@gmail.com>
2020-09-18 02:00:18 +0100

    🚀 This commit update navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaTimes, FaBars } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
 
 import * as S from './styles';
+import { Button } from '../../globalStyles';
 
 function Navbar() {
   const [clicked, setClicked] = useState(false);
-
+  const [button, setButton] = useState(true);
+  
   const handleClick = () => setClicked(!clicked);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  }
+
+  useEffect(() => {
+    setButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+  
+  
   return (
     <IconContext.Provider value={{color: '#FFF'}}>
       <S.Nav>
@@ -29,6 +47,19 @@ function Navbar() {
           <S.NavItem>
             <S.NavLinks to="/products">Products</S.NavLinks>
           </S.NavItem>
+          <S.NavItemBtn>
+            {button ? (
+              <S.NavBtnLink to="/sign-up">
+                <Button primary>SignUp</Button>
+              </S.NavBtnLink>
+              ) : (
+              <S.NavBtnLink to="/sign-up">
+                <Button fontBig primary>
+                SignUp
+              </Button>
+            </S.NavBtnLink>
+          )}
+          </S.NavItemBtn>
         </S.NavMenu>
       </S.NavbarContainer>
       </S.Nav>
